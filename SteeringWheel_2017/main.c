355430f16654c5f7f3db7ff7340fed34b44c7d1b
DESKTOP-QP1DVTO\Sondre <sondreixix@gmail.com>
2017-03-18 15:17:28 +0100

    Added Atmel Studio project and UM driver submodule

---
+++ SteeringWheel_2017/main.c
@@ -0,0 +1,18 @@
+/*
+ * SteeringWheel_2017.c
+ *
+ * Created: 18/03/17 15:13:44
+ * Author : Sondre
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
