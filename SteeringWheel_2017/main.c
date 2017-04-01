bdd2a8e8a65df392bb258a474f9e7b24977782a0
Ola Lium <olalium@gmail.com>
2017-04-01 16:53:38 +0200

    Button implementation for steering wheel UM
    *You can see that a button is pressed with printf.
    *Plug-and-play, upload this to a um and connect it to steering wheel and
    you are set.
    *TODO: Implement adc driver to get input from joystick and throttles

--- SteeringWheel_2017/main.c
+++ SteeringWheel_2017/main.c
@@ -6,13 +6,47 @@
  */ 
 
 #include <avr/io.h>
-
+// #include "adc.h"
+#include "usbdb.h"
+#include <util/delay.h>
 
 int main(void)
 {
-    /* Replace with your application code */
-    while (1) 
-    {
-    }
+	// adc_init();
+	PORTB |= (1<<PB4); //Initiate (input) PE3, indicator left
+	DDRB &= ~(1<<PB4);
+	
+	PORTB |= (1<<PB3); //Initiate (input) PE4, indicator right
+	DDRB &= ~(1<<PB3);
+	
+	PORTB |= (1<<PB1); //Initiate (input) PB4, horn
+	DDRB &= ~(1<<PB1);
+	
+	PORTB |= (1<<PB2); //Initiate (input) PB4, CRUISE CONTROL
+	DDRB &= ~(1<<PB2);
+	
+	PORTB |= (1<<PB0); //Initiate (input) PB4, joy button
+	DDRB &= ~(1<<PB0);
+	
+	usbdbg_init();
+	
+	while (1)
+	{
+		if (!((1<<PINB0)&PINB)) {
+			printf("joy button ;) \n");
+		}
+		if (!((1<<PINB1)&PINB)) {
+			printf("horn \n");
+		}
+		if (!((1<<PINB2)&PINB)) {
+			printf("cruise control \n");
+		}
+		if (!((1<<PINB3)&PINB)) {
+			printf("indicator right \n");
+		}
+		if (!((1<<PINB4)&PINB)) {
+			printf("indicator left \n");
+		}
+		_delay_ms(10);
+	}
 }
-
